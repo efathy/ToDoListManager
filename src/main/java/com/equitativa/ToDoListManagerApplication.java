04498bf224b1004ab502579e1d9ef018e90ecbb6
eslamfathy3000 <eslam.fathy3000@gmail.com>
2021-01-30 18:41:07 +0200

    Integrating the needed dependencies * Hibernate * Java Validators * Wicket-Bootstrap * Postgres connector

--- src/main/java/com/equitativa/ToDoListManagerApplication.java
+++ src/main/java/com/equitativa/ToDoListManagerApplication.java
@@ -1,10 +1,17 @@
 package com.equitativa;
 
 import com.equitativa.pages.Home;
+import de.agilecoders.wicket.core.Bootstrap;
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class ToDoListManagerApplication extends WebApplication {
+
+    @Override
+    protected void init() {
+        super.init();
+        Bootstrap.install(this);
+    }
 
     @Override
     public Class<? extends Page> getHomePage() {
